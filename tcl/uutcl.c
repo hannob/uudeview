10f992d0da673e186b2213f6d3c953366a3c4085
Hanno Böck <hanno@gentoo.org>
2022-12-01 08:29:16 +0100

    049_glibc_2.27.diff from Debian.
    Description: Remove a hack for Sun that causes FTBFS with glibc >= 2.27
    Author: Adrian Bunk <bunk@debian.org>

--- tcl/uutcl.c
+++ tcl/uutcl.c
@@ -48,14 +48,6 @@
 #include <tcl.h>
 #endif
 
-/*
- * The following variable is a special hack that is needed in order for
- * Sun shared libraries to be used for Tcl.
- */
-
-extern int matherr();
-int *tclDummyMathPtr = (int *) matherr;
-
 #include <uudeview.h>
 #include <uuint.h>
 #include <fptools.h>
