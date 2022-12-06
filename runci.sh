1383e10c6ae96dcbde059b78a90fb259ba46adfd
Hanno Böck <hanno@gentoo.org>
2022-12-06 14:17:53 +0100

    add MSAN and UBSAN tests to CI

--- runci.sh
+++ runci.sh
@@ -4,6 +4,22 @@ set -euo pipefail
 autoconf
 
 # Test with ASAN / Address Sanitizer
-./configure CFLAGS="-fsanitize=address" LDFLAGS="-fsanitize=address"
+export ASAN_OPTIONS="abort_on_error=1"
+./configure CFLAGS="-fsanitize=address -U_FORTIFY_SOURCE" LDFLAGS="-fsanitize=address -U_FORTIFY_SOURCE"
+make clean
+make
+make check
+
+# Test with clang and MSAN / Memory Sanitizer
+export MSAN_OPTIONS="abort_on_error=1"
+./configure CC=clang LD=clang CFLAGS="-fsanitize=memory -U_FORTIFY_SOURCE" LDFLAGS="-fsanitize=memory -U_FORTIFY_SOURCE"
+make clean
+make
+make check
+
+# Test with clang and UBSAN / Undefined Behavior Sanitizer
+export UBSAN_OPTIONS="halt_on_error=1:abort_on_error=1"
+./configure CC=clang LD=clang CFLAGS="-fsanitize=undefined" LDFLAGS="-fsanitize=undefined"
+make clean
 make
 make check
