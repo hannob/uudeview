be82c528bc8e64a0440a00595dc536a3ddf66e1c
Hanno Böck <hanno@gentoo.org>
2022-12-07 11:30:32 +0100

    Allow good and bad test files

--- test/test.sh
+++ test/test.sh
@@ -4,12 +4,22 @@ set -euo pipefail
 MYDIR="$(dirname "$0")"
 TMPD="$(mktemp -d)"
 
-for fn in "$MYDIR"/*.msg; do
+# All test files ending with .good.msg contain the
+# file doc/test.txt with various encodings.
+for fn in "$MYDIR"/*.good.msg; do
 	"$MYDIR"/../unix/uudeview -i -p "$TMPD" "$fn"
 
 	diff "$MYDIR"/../doc/test.txt "$TMPD"/test.txt
 
 	rm "$TMPD"/test.txt
+done
+
+# The files ending with .bad.msg are sample files
+# triggering bugs like memory safety violations. They
+# do not contain valid data, thus we do not expect
+# extracted files, we just do not want them to crash.
+for fn in "$MYDIR"/*.bad.msg; do
+	"$MYDIR"/../unix/uudeview -i -p "$TMPD" "$fn"
 done
 
 rmdir "$TMPD"
